bcfba6acca2582d9f07bf1157b66f132414fbd65
FanKiing <essayhyasser@gmail.com>
2025-10-07 11:46:00 +0100

    additional work

---
+++ React/react-tp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Q1.jsx'
+import Counter from './Q1.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>Mon Application</h1>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
